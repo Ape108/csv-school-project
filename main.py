7011e476156f6fabaed1bb0c156898b31335f810
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2024-10-04 13:51:08 -0500

    Finished Product

--- main.py
+++ main.py
@@ -4,26 +4,55 @@ Cameron & Ashton
 import csv
 
 def salaryAvg(csv_file):
+    total = 0
+    terms = 0
     salary_reader = csv.reader(csv_file, delimiter=",")
     for row in salary_reader:
         if row == ["Name", "Salary", "Sales"]:
             continue
-        print(row)
+        total += int(row[1])
+        terms += 1
+    average = round(total / terms, 2)
+    return average
+
+def maxRecord(csv_file):
+    maxValue = 0
+    name = None
+    sales_reader = csv.reader(csv_file, delimiter=",")
+    for row in sales_reader:
+        if row == ["Name", "Salary", "Sales"]:
+            continue
+        if maxValue < int(row[2]):
+            maxValue = int(row[2])
+            name = row[0]
+    return (maxValue, name)
 
 file_name = ''
 while file_name == '':
     file_name = input("Please Enter the name of the file: ")
     try:
         with open(file_name, "r") as my_file:
-            salaryAvg(my_file)
-            pass
+            choice = None
+            while choice == None:
+                choice = input("Would you like to calculate the average salary, or the highest sales record?\nPlease Enter 1 or 2: ")
+                if choice == '1':
+                    salary_average = salaryAvg(my_file)
+                    print(f"The average salary is: \n${salary_average}")
+                elif choice == '2':
+                    highest_record, name = maxRecord(my_file)
+                    print(f"{name} had the highest sales record: \n${highest_record}")
+                    pass
+                else:
+                    print("Please enter 1 or 2.")
+                    choice = None
 
     except (FileNotFoundError, IOError):
         print("File does not exist. Please enter a valid file name.")
         file_name = ''
+    """
     except:
         print("Error retrieving file. Please try again.")
         file_name = ''
-
+    """
 
 
